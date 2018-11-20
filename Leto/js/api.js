2320324206c1de043654b80c0d0367250d0f3ea4
Thibaut <thibaut.monesma@gmail.com>
2018-11-20 13:51:10 +0100

    api request

---
+++ Leto/js/api.js
@@ -0,0 +1,79 @@
+function findCityWithQuery(query) {
+    $.ajax({
+    url: "https://api.internationalshowtimes.com/v4/cities?query="+query,
+    type: "GET",
+    datatype: "json",
+    data: {
+        "countries": "FR",
+    },
+    headers: {
+        "X-API-Key": "nce8u3Rq5yNq0jL9FjpmxZ8jWCzv9xvw",
+    },
+    })
+    .done(function(response, textStatus, jqXHR) {
+        console.log(response);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+    });   
+}
+
+function findShowtimesByCity(cityId, movieId, date) {
+    $.ajax({
+    url: "https://api.internationalshowtimes.com/v4/showtimes?city_ids="+cityId+"&movie_id="+movieId+"&time_to="+date,
+    type: "GET",
+    datatype: "json",
+    data: {
+        "countries": "FR",
+    },
+    headers: {
+        "X-API-Key": "nce8u3Rq5yNq0jL9FjpmxZ8jWCzv9xvw",
+    },
+    })
+    .done(function(response, textStatus, jqXHR) {
+        console.log(response);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+    });   
+}
+function findCinemaById(cineId) {
+    $.ajax({
+    url: "https://api.internationalshowtimes.com/v4/cinema/"+cityId,
+    type: "GET",
+    datatype: "json",
+    data: {
+        "countries": "FR",
+    },
+    headers: {
+        "X-API-Key": "nce8u3Rq5yNq0jL9FjpmxZ8jWCzv9xvw",
+    },
+    })
+    .done(function(response, textStatus, jqXHR) {
+        console.log(response);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+    });   
+}
+function displayMovieWithId(id) {
+    $.ajax({
+    url: "https://api.internationalshowtimes.com/v4/movies/"+id,
+    type: "GET",
+    datatype: "json",
+    data: {
+        "countries": "FR",
+    },
+    headers: {
+        "X-API-Key": "nce8u3Rq5yNq0jL9FjpmxZ8jWCzv9xvw",
+    },
+    })
+    .done(function(response, textStatus, jqXHR) {
+        console.log(response);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+    });
+}
+
+findCityWithQuery('tou');

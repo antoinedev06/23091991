117d798393328074d49310699c8b2d3f58c3307b
“Cyrhades” <cyrhades76@gmail.com>
2018-11-21 15:27:34 +0100

    reparation code de trop

--- Leto/js/api.js
+++ Leto/js/api.js
@@ -1,3 +1,6 @@
+// cette fonction fait un appel ajax 
+// en fonction d'un mot clef
+
 function findCityWithQuery(query) {
     $.ajax({
     url: "https://api.internationalshowtimes.com/v4/cities?query="+query,
@@ -10,52 +13,20 @@ function findCityWithQuery(query) {
         "X-API-Key": "nce8u3Rq5yNq0jL9FjpmxZ8jWCzv9xvw",
     },
     })
-    .done(function(response, textStatus, jqXHR) {
-        console.log(response);
-    })
+    .done(displayCities)
     .fail(function(jqXHR, textStatus, errorThrown) {
         console.log("HTTP Request Failed");
     });   
 }
 
-<<<<<<< HEAD
-var cityChoose = document.getElementById('search');
-
-var cityDisplay = document.querySelector('ul');
-
-cityChoose.onchange = function() {
-  var city = cityChoose.value;
-  updateDisplay(city);
-};
-
-function updateDisplay(city) {
-
-city = city.replace(" ", "");
-city = city.toLowerCase();
-var url = findCityWithQuery(city);
-
-request.onload = function() {
- cityDisplay.textContent = request.response;
-};
-request.send();
-
-updateDisplay('');
-cityChoose.value = '';
-};
-
-/*------------*/
-$(function() {
-   $( "#search").autocomplete({
-         source: findCityWithQuery(),
-         minLength: 2,
-   });
-});
+function displayCities() {
+    console.log(response);
+}
 
 
+/*
+**** On a pas encore besoin de ça ****
 
-
-=======
->>>>>>> 774ac65933740b414a84b682dac0a8aa3d7e326d
 function findShowtimesByCity(cityId, movieId, date) {
     $.ajax({
     url: "https://api.internationalshowtimes.com/v4/showtimes?city_ids="+cityId+"&movie_id="+movieId+"&time_to="+date,
@@ -75,10 +46,7 @@ function findShowtimesByCity(cityId, movieId, date) {
         console.log("HTTP Request Failed");
     });   
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 774ac65933740b414a84b682dac0a8aa3d7e326d
 function findCinemaById(cineId) {
     $.ajax({
     url: "https://api.internationalshowtimes.com/v4/cinema/"+cityId,
@@ -98,13 +66,8 @@ function findCinemaById(cineId) {
         console.log("HTTP Request Failed");
     });   
 }
-<<<<<<< HEAD
 
 function displayMovieWithId(id) {
-
-=======
-function displayMovieWithId(id) {
->>>>>>> 774ac65933740b414a84b682dac0a8aa3d7e326d
     $.ajax({
     url: "https://api.internationalshowtimes.com/v4/movies/"+id,
     type: "GET",
@@ -122,11 +85,7 @@ function displayMovieWithId(id) {
     .fail(function(jqXHR, textStatus, errorThrown) {
         console.log("HTTP Request Failed");
     });
-<<<<<<< HEAD
 
 }
-=======
-}
 
-findCityWithQuery('tou');
->>>>>>> 774ac65933740b414a84b682dac0a8aa3d7e326d
+*/
--- Leto/js/events.js
+++ Leto/js/events.js
@@ -1,35 +1,12 @@
-
-
-$('#nav').on('click', displayMenu);
- $('#nav2').on('click', displayMenu);
- function displayMenu(){
+function displayMenu(){
   $('#nav').toggleClass('hidden');
   $('#menu').toggleClass('hidden');
   $('#nav2').toggleClass('hidden');
 }
-<<<<<<< HEAD
 
 
 
-=======
-/*
-$('#search').autocomplete({
-    source : function(requete, reponse){ // les deux arguments représentent les données nécessaires au plugin
-	$.ajax({
-            url : 'http://ws.geonames.org/searchJSON', // on appelle le script JSON
-            dataType : 'json', // on spécifie bien que le type de données est en JSON
-            data : {
-                name_startsWith : $('#search').val() // on donne la chaîne de caractère tapée dans le champ de recherche
-            },
-            
-            success : function(donnee){
-                reponse($.map(donnee.geonames, function(objet){
-                    return objet.name + ', ' + objet.countryName; // on retourne cette forme de suggestion
-                }));
-            }
-        });
-    }
-});
 
-/*
->>>>>>> 774ac65933740b414a84b682dac0a8aa3d7e326d
+$('#nav').on('click', displayMenu);
+$('#nav2').on('click', displayMenu);
+
